3ae1e4a32b3684e2062ffafddc67ae9965da2442
Zsuzsanna Seredi-Szabo <zseredi-@student.42.fr>
2020-04-06 16:58:35 +0200

    cleaned up the code

--- UnitConvert/Controller/ViewController.swift
+++ UnitConvert/Controller/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var lengthValue: UITextField!
     
     var lengthStatus:Bool = true
-    var weightStatus:Bool = true 
+    var weightStatus:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,74 +40,69 @@ class ViewController: UIViewController {
         weightValue.addTarget(self, action: #selector(calcWeight), for: UIControl.Event.editingChanged)
     }
     
+    func changeTextColor(status: Bool, label1: UILabel, label2: UILabel) {
+        if status {
+            label1.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            label2.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        } else {
+            label1.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            label2.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
     @objc func toggleLength(srcObj: UISwitch) {
         lengthStatus = srcObj.isOn
-        if lengthStatus {
-            kmMlLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            mlKmLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        } else {
-            mlKmLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            kmMlLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        }
+        self.changeTextColor(status: lengthStatus, label1: kmMlLabel, label2: mlKmLabel)
         self.calcLength(srcObj: lengthValue)
     }
     
     @objc func toggleWeight(srcObj: UISwitch) {
         weightStatus = srcObj.isOn
-        if weightStatus {
-            kgLbLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            lbKgLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        } else {
-            kgLbLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-            lbKgLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        }
+        self.changeTextColor(status: weightStatus, label1: kgLbLabel, label2: lbKgLabel)
         self.calcWeight(srcObj: weightValue)
     }
     
-    @objc func calcLength(srcObj: UITextField) {
-        var measure:String = " Ml"
-        var lengthResult = 0.0
-        if Double(srcObj.text!) == nil {
-        lengthResultField.text = "Invalid value"
-        return
+    func isNumberValid(inputText: String?, value: Double, label: UILabel) -> Bool {
+        if (inputText != "") && Double(inputText!) == nil {
+            label.text = "Invalid value"
+            return false
         }
-        let value = Double(srcObj.text!) ?? 0.0
         if value < 0 {
-            lengthResultField.text = "Use a positive number!"
+            label.text = "Use a positive number!"
+            return false
+        }
+        return true
+    }
+    
+    func mathOperation(status: Bool, value: Double, number:Double) -> Double {
+        var result = 0.0
+        if status {
+            result = value * number
+        } else {
+            result = value / number
+        }
+        return(Double(round(result * 100) / 100))
+    }
+    
+    @objc func calcLength(srcObj: UITextField) {
+        let value = Double(srcObj.text!) ?? 0.0
+        if !self.isNumberValid(inputText: srcObj.text, value: value, label: lengthResultField) {
             return
         }
-        
-        if lengthStatus {
-            lengthResult = value * 0.6214
-        } else {
-            lengthResult = value / 0.6214
-            measure = " Km"
-        }
-        lengthResult = Double(round(lengthResult * 100) / 100)
+        let lengthResult = self.mathOperation(status: lengthStatus, value: value, number: 0.6214)
+        let measure = lengthStatus ? " Ml" : " Km"
         lengthResultField.text = String(lengthResult) + measure
         
     }
 
     @objc func calcWeight(srcObj: UITextField) {
-        var measure:String = " Lb"
-        var weightResult = 0.0
-        if Double(srcObj.text!) == nil {
-            weightResultField.text = "Invalid value"
-            return
-        }
         let value = Double(srcObj.text!) ?? 0.0
-       
-        if value < 0 {
-            weightResultField.text = "Use a positive number!"
+        
+        if !isNumberValid(inputText: srcObj.text, value: value, label: weightResultField) {
             return
         }
-        if weightStatus {
-            weightResult = value / 0.45359237
-        } else {
-            weightResult = value * 0.45359237
-            measure = " Kg"
-        }
-        weightResult = Double(round(weightResult * 100) / 100)
+        let weightResult = mathOperation(status: weightStatus, value: value, number: 2.20462262185)
+        let measure = weightStatus ? " Lb" : " Kg"
         weightResultField.text = String(weightResult) + measure
     }
 
